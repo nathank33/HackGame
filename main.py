9f9191bc43c3255c2b32f2c824ee72e8d96c03e8
nathank33 <nathanbraunguitar@gmail.com>
2014-10-18 15:20:39 -0700

    Added ball, adjusted main

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import pygame
 import time
+import os
+from pygame.locals import *
 
 jump_speed = 3
 width, height = 640, 480
 gravity = -0.5
 all_objects = []
 
+def load_png(name):
+        """ Load image and return image object"""
+        fullname = os.path.join('data', name)
+        try:
+                image = pygame.image.load(fullname)
+                if image.get_alpha is None:
+                        image = image.convert()
+                else:
+                        image = image.convert_alpha()
+        except pygame.error as message:
+                print('Cannot load image:', fullname)
+                raise SystemExit(message)
+        return image, image.get_rect()
+
+
 class MoveableSprite(pygame.sprite.Sprite):
 	def __init__(self):
 		pygame.sprite.Sprite.__init__(self)
-		#self.image, self.rect = load_png('ball.png') # Change this
+		self.image, self.rect = load_png('ball.png')
 		self.x, self.y = 0, 0
 		self.speedx, self.speedy = 0, 0
 
 	def update(self):
 		self.x += self.speedx
 		self.y += self.speedy
+		self.rect.move([self.x, self.y])
 
 class Player(MoveableSprite):
 	def __init__(self):
@@ -34,30 +52,29 @@ class Bullet(MoveableSprite):
 
 def main():
 	#Initialize Pygame
-	player = Player('Hero')
+	
 	pygame.init()
-    screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption('Hack Game')
+	screen = pygame.display.set_mode((width, height))
+	pygame.display.set_caption('Hack Game')
 
 	background = pygame.Surface(screen.get_size())
-    background = background.convert()
-    background.fill((0, 0, 0))
+	background = background.convert()
+	background.fill((0, 0, 0))
 
     # Initialise clock
-    clock = pygame.time.Clock()
+	clock = pygame.time.Clock()
 
-    while True:
-    	clock.tick(60)
-    	for obj in all_objects:
-    		objects.update()
+	player = Player()
+	while True:
+		clock.tick(60)
+		for obj in all_objects:
+			objects.update()
 
-    	for event in pygame.event.get():
-    		if event.type == QUIT:
-    			return
-    		elif event.type == JUMP and player.can_jump:
-    			player.jump()
-    		#elif event.type == SHOOT and player.can_shoot:
-    		#	player.shoot()
-
-
-    	# Handle all of the events
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				return
+			#elif event.type == JUMP and player.can_jump:
+			#	player.jump()
+			#elif event.type == SHOOT and player.can_shoot:
+			#	player.shoot()
+main()
