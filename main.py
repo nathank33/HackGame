3a1919e107a48bb9a3cc800bea803bec4bb9723b
nathank33 <nathanbraunguitar@gmail.com>
2014-10-18 14:38:52 -0700

    Initial creation of main.py,

---
+++ main.py
@@ -0,0 +1,39 @@
+import pygame
+
+class MoveableSprite(pygame.sprite.Sprite):
+	def __init__(self):
+		pygame.sprite.Sprite.__init__(self)
+		#self.image, self.rect = load_png('ball.png') # Change this
+		self.x, self.y = 0, 0
+		self.speedx, self.speedy = 0, 0
+
+class Player(MoveableSprite):
+	def __init__(self):
+		MoveableSprite.__init__(self)
+
+	def jump(self):
+		print("Handle Jump")
+
+	def shoot(self):
+		print("Handle Shooting")
+	
+
+class Bullet(MoveableSprite):
+	def __init__(self):
+		MoveableSprite.__init__(self)
+
+width, height = 640, 480
+def main():
+	#Initialize Pygame
+	pygame.init()
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption('Hack Game')
+
+	background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((0, 0, 0))
+
+    while True:
+    	clock.tick(60)
+
+    	# Handle all of the events
